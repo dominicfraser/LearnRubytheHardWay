af29115b8bdc4e70893050c890d6a113bc5d2120
Dominic <mrdjfraser@gmail.com>
2017-02-20 23:18:56 +0000

    first working draft of character class

--- TextAdventure1/character_class.rb
+++ TextAdventure1/character_class.rb
@@ -1,45 +1,31 @@
-class CharacterType 
+class Character
+
+  attr_accessor :base_strength
+  attr_accessor :base_health
+  attr_accessor :base_agility 
 
   def initialize(type)
     @type = type
+    @base_strength = 0
+    @base_health = 0
+    @base_agility = 0
+
+
+    case @type 
+    when "brute"
+      @base_strength += 70
+      @base_health += 60
+      @base_agility += 40
+    when "rouge"
+      @base_strength += 50
+      @base_health += 50
+      @base_agility += 70
+    when "cleric"
+      @base_strength += 40
+      @base_health += 80
+      @base_agility += 50
+    else
+      puts "Please select brute, rouge, or cleric." 
+    end
   end
-
-  base_strength = 0
-  base_health = 0
-  base_agility = 0
-
-  if type == brute {
-    base_strength += 70
-    base_health += 60
-    base_agility += 40
-  elsif type == rouge
-    base_strength += 50
-    base_health += 50
-    base_agility += 70
-  elsif
-    type == cleric
-    base_strength += 40
-    base_health += 80
-    base_agility += 50
-  else
-    puts "There has been an error creating this character" 
-    }
-
-end
-
-
-def Player < CharacterType
-
-  def initialize()
-  end
-
-  base_strength
-  base_health
-  base_agility
-
-
-  damage = 
-  defence =
-  agility =
-
 end
