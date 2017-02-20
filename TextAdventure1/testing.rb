b094885b4d371a9eccf3cdc256b23b70f9daeb7c
Dominic <mrdjfraser@gmail.com>
2017-02-20 22:03:05 +0000

    changes to character test

--- TextAdventure1/testing.rb
+++ TextAdventure1/testing.rb
@@ -2,33 +2,38 @@ class Character
 
   def initialize(type)
     @type = type
+    @base_strength = 0
+    @base_health = 0
+    @base_agility = 0
+    
+    case @type 
+    when @type == "brute"
+      base_strength += 70
+      base_health += 60
+      base_agility += 40
+    when @type == "rouge"
+      base_strength += 50
+      base_health += 50
+      base_agility += 70
+    when @type == "cleric"
+      base_strength += 40
+      base_health += 80
+      base_agility += 50
+    else
+      puts "There has been an error creating this character" 
+    end
   end
 
-  @base_strength = 0
-  base_health = 0
-  base_agility = 0
-
-  case @type 
-  when @type == "brute"
-    base_strength += 70
-    base_health += 60
-    base_agility += 40
-  when @type == "rouge"
-    base_strength += 50
-    base_health += 50
-    base_agility += 70
-  when @type == "cleric"
-    base_strength += 40
-    base_health += 80
-    base_agility += 50
-  else
-    puts "There has been an error creating this character" 
+  def base_strength
+    return @base_strength
   end
+
+
 end
 
 puts "Please choose a character type:"
 chartypeselection = gets.chomp
-Character.new("chartypeselection")
+Jake = Character.new(chartypeselection)
 puts "Great!"
-puts "Your Base Strength is #{base_strength}"
+puts "Your Base Strength is #{Jake.base_strength}"
 
